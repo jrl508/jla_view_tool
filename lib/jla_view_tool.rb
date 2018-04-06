83c89984aeeef1d0fd79a36ae2ba29ce10624eda
jrl508 <jrl47@yahoo.com>
2018-04-06 03:24:09 -0400

    Dev tool initial commit

---
+++ lib/jla_view_tool.rb
@@ -0,0 +1,6 @@
+require "jla_view_tool/version"
+require "jla_view_tool/renderer"
+
+module JlaViewTool
+  # Your code goes here...
+end
---
+++ lib/jla_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JlaViewTool
+  VERSION = "0.1.0"
+end
